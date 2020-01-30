539278833b037382e0ff4996c4b1cd4762f62d63
qiqiqiqqq <2523173152@qq.com>
2020-01-30 12:04:33 +0800

    项目结构创建完成。。。

---
+++ gmall-admin-web/src/test/java/com/atguigu/gmall/gmalladmin/web/GmallAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.gmalladmin.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallAdminWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
